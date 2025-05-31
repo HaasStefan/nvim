39b92fbef1720a4e46301192e46209b8e4db101e
Stefan Haas <stefan.haas.privat@gmail.com>
2025-05-31 21:54:36 +0200

    NeoVim dashboard font

--- lua/config/lazy.lua
+++ lua/config/lazy.lua
@@ -5,7 +5,7 @@ if not (vim.uv or vim.loop).fs_stat(lazypath) then
   if vim.v.shell_error ~= 0 then
     vim.api.nvim_echo({
       { "Failed to clone lazy.nvim:\n", "ErrorMsg" },
-      { out,                            "WarningMsg" },
+      { out, "WarningMsg" },
       { "\nPress any key to exit..." },
     }, true, {})
     vim.fn.getchar()
@@ -18,13 +18,14 @@ require("lazy").setup({
   spec = {
     -- Add LazyVim and import its plugins
     { "LazyVim/LazyVim", import = "lazyvim.plugins" },
+    { "folke/snacks.nvim", opts = { dashboard = { enabled = false } } },
     -- Add GitHub theme plugin
     {
       "projekt0n/github-nvim-theme",
       config = function()
         -- Use the even darker GitHub theme
         vim.cmd("colorscheme github_dark_default")
-      end
+      end,
     },
     -- Import/override with your plugins
     { import = "plugins" },
@@ -33,12 +34,12 @@ require("lazy").setup({
     -- By default, only LazyVim plugins will be lazy-loaded. Your custom plugins will load during startup.
     lazy = false,
     -- It's recommended to leave version=false for now, since many plugins supporting versioning have outdated releases.
-    version = false,                                     -- always use the latest git commit
+    version = false, -- always use the latest git commit
   },
   install = { colorscheme = { "github_dark_default" } }, -- Install the darker GitHub theme
   checker = {
-    enabled = true,                                      -- Automatically check for plugin updates
-    notify = false,                                      -- Do not notify on update
+    enabled = true, -- Automatically check for plugin updates
+    notify = false, -- Do not notify on update
   },
   performance = {
     rtp = {
